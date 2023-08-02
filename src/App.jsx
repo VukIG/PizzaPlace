1ddd84cf4f2c8c1e5286c3d172df17d756e96ecf
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-02 12:36:40 +0200

    'React icons umesto fontawsome'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-red "></div>
+      <div className="flex bg-[#1E1E1E] ">
+        <div className="">
+          <img src="logo.svg" alt="" />
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">View</a></li>
+          </ul>
+        </div>
+        <div className="">
+          <button><i>ovde ikonica</i> Cart</button>
+        </div>
+      </div>
+      <div className="flex">
+          <div className="flex horizontal">
+            <h1>Freshly baked Pizzas, made with love</h1>
+            <p>Discover a gastronomic delight of Exquisite Pizzas, crafted with the finest ingredients and careful attention to detail</p>
+            <button>Start ordering</button>
+          </div>
+          <div className="">
+            <img src="" alt="" />
+          </div>
+      </div>
     </>
   )
 }
