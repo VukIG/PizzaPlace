4bbf0c15f860068af0ed28b94aa4f0733f0801de
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-02 14:23:14 +0200

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import Logo from './assets/logo.svg'
+import { useState } from "react";
+import Logo from "./assets/logo.svg";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="flex px-[15px] py-[10px] bg-[#1E1E1E] ">
         <div className="">
           <Logo />
-          <ul className='inline'>
-            <li><a href="" className=' p-[10px] font-[400] font-' >Home</a></li>
-            <li><a href="" className=' p-[10px] '>View</a></li>
+          <ul className="inline">
+            <li>
+              <a href="" className=" p-[10px] font-[400] font-">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="" className=" p-[10px] ">
+                View
+              </a>
+            </li>
           </ul>
         </div>
         <div className="">
-          <button><i>ovde ikonica</i> Cart</button>
+          <button>
+            <i>ovde ikonica</i> Cart
+          </button>
         </div>
       </div>
       <div className="flex">
-          <div className="flex horizontal">
-            <h1>Freshly baked Pizzas, made with love</h1>
-            <p>Discover a gastronomic delight of Exquisite Pizzas, crafted with the finest ingredients and careful attention to detail</p>
-            <button>Start ordering</button>
-          </div>
-          <div className="">
-            <img src="" alt="" />
-          </div>
+        <div className="flex horizontal">
+          <h1>Freshly baked Pizzas, made with love</h1>
+          <p>
+            Discover a gastronomic delight of Exquisite Pizzas, crafted with the
+            finest ingredients and careful attention to detail
+          </p>
+          <button>Start ordering</button>
+        </div>
+        <div className="">
+          <img src="" alt="" />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
