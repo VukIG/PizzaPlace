bb420a54fb21006fff455c2bf456b75d62ca585b
Dejan Milovanovic <dejan.milovanovic@codeup.rocks>
2023-08-03 11:23:35 +0200

    Font size class changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import Header from "./Header";
 import Button from "./Button";
+import Header from "./Header";
 function App() {
   return (
     <>
@@ -9,7 +8,7 @@ function App() {
         <div className="flex p-[25px] justify-center items-center gap-[25px]">
           <div className="flex flex-col break-words">
             <h1 className="font-[700] text-[48px] leading-[48px]">Freshly baked Pizzas, made with love</h1>
-            <p className="text-[16px]	">
+            <p className="text-base	">
               Discover a gastronomic delight of Exquisite Pizzas, crafted with the
               finest ingredients and careful attention to detail
             </p>
