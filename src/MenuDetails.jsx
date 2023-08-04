22370b741640e662b271b0c81e5122ec48efbb27
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-04 14:06:05 +0200

    Nove komponente za menu details

---
+++ src/MenuDetails.jsx
@@ -0,0 +1,13 @@
+function MenuDetails() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div className="flex">
+        <h1>Vegan Veggie</h1>
+        <p>Daiya vegan mozzarella, paired with fresh veggies</p>
+      </div>
+    </div>
+  );
+}
+
+export default MenuDetails;
