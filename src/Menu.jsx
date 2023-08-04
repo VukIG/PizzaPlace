0f1ba6fee1974dcffb5cf4f0a363c981523f82e1
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-04 13:12:55 +0200

    Dodata + ikonica

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,15 +1,15 @@
 import MenuItem from "./MenuItem";
-import { HiPlusSm } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 
 function Menu() {
   return (
     <div className="w-full h-full bg-slate-200 p-5">
       <h1 className="text-4xl mb-5 font-bold">Discover menu</h1>
-      <MenuItem name="Order now" icon={<HiPlusSm />} /> 
-      <MenuItem name="Order now" icon={<HiPlusSm />} /> 
-      <MenuItem name="Order now" icon={<HiPlusSm />} /> 
-      <MenuItem name="Order now" icon={<HiPlusSm />} /> 
-      <MenuItem name="Order now" icon={<HiPlusSm />} /> 
+      <MenuItem name="Order now" icon={<FaPlus />} /> 
+      <MenuItem name="Order now" icon={<FaPlus />} /> 
+      <MenuItem name="Order now" icon={<FaPlus />} /> 
+      <MenuItem name="Order now" icon={<FaPlus />} /> 
+      <MenuItem name="Order now" icon={<FaPlus />} /> 
     </div>
   );
 }
