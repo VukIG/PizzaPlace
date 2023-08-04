69a7731f0d535857fdeed88fee6d4b9795c16a68
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-04 10:53:22 +0200

    Menu layout zavrsen

--- src/Item.jsx
+++ src/Item.jsx
@@ -2,14 +2,19 @@ import Button from "./Button";
 function Item() {
   return (
     <div>
-      <div className="flex align-middle items-center">
-        <img src="" alt="" />
-        <div className="flex flex-col justify-start align-middle items-center">
-          <h1>Vegan veggie</h1>
-          <p>Daiya vegan pizza, paired with fresh veggies</p>
-          <h1>$150</h1>
+      <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-[25px]">
+        <div className="flex items-center gap-5">
+          <img className="w-[100px] h-[100px] my-5 ml-5 rounded-lg" src="../public/pizzaBanner.jpeg" alt="" />
+          <div className="flex flex-col justify-start items-start ">
+            <h1 className="text-xl font-bold">Vegan veggie</h1>
+            <p className="py-3 text-sm italic" >Daiya vegan pizza, paired with fresh veggies</p>
+            <h1 className="font-bold text-orange-400">$150</h1>
+          </div>
         </div>
-        <Button />
+        
+        <div className="mr-5">
+          <Button />
+        </div>
       </div>
     </div>
   );
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,8 +1,8 @@
 import Item from "./Item";
 function Menu() {
   return (
-    <div>
-      <h1>Discover menu</h1>
+    <div className="w-full h-full bg-slate-200 p-[25px]">
+      <h1 className="text-4xl mb-5 font-bold">Discover menu</h1>
       <Item />
       <Item />
       <Item />
