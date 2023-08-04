678f47609f5ed96084ecfed017a7844c4a4a0ba9
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-04 12:48:18 +0200

    Preimenovao Item.jsx u MenuItem i ispravio klase

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,13 +1,13 @@
-import Item from "./Item";
+import MenuItem from "./MenuItem";
 function Menu() {
   return (
-    <div className="w-full h-full bg-slate-200 p-[25px]">
+    <div className="w-full h-full bg-slate-200 p-5">
       <h1 className="text-4xl mb-5 font-bold">Discover menu</h1>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+      <MenuItem /> 
+      <MenuItem />     
+      <MenuItem />     
+      <MenuItem />     
+      <MenuItem />     
     </div>
   );
 }
--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 function Item() {
   return (
     <div>
-      <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-[25px]">
+      <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-5">
         <div className="flex items-center gap-5">
           <img
             className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
