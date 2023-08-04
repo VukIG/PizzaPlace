1ee20a4ca8280616c0da63258737b666ef4f796d
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-04 14:06:49 +0200

    MenuDetails komponenta html zavrsen

--- src/MenuDetails.jsx
+++ src/MenuDetails.jsx
@@ -1,13 +1,23 @@
+
 function MenuDetails() {
   return (
     <div>
-      <img src="" alt="" />
-      <div className="flex">
-        <h1>Vegan Veggie</h1>
-        <p>Daiya vegan mozzarella, paired with fresh veggies</p>
-      </div>
+        <img src="" alt="" />
+        <div className="flex">
+            <h1>Vegan Veggie</h1>
+            <p>Daiya vegan mozzarella, paired with fresh veggies</p>
+           <div className="">
+           <h1>Topings:</h1>
+                <ul>  
+                    <li>Mozzarela</li>
+                    <li>Tomatto</li>
+                    <li>Onion</li>
+                    <li>Mushrooms</li>
+            </ul>
+           </div>
+        </div>
     </div>
-  );
+  )
 }
 
-export default MenuDetails;
+export default MenuDetails
