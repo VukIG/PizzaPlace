bd7aae78aa4d8e9a2487ebb82e5fb013bde5940e
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-23 14:35:21 +0200

    Dodat mock-data fajl

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,11 +5,11 @@ function Menu() {
   return (
     <div className="w-full flex flex-col justify-start h-full bg-slate-200 p-5 mb-5">
       <h1 className="text-4xl mb-10 font-bold">Discover menu</h1>
-      <MenuItem name="Order now" icon={<FaPlus />} />
-      <MenuItem name="Order now" icon={<FaPlus />} />
-      <MenuItem name="Order now" icon={<FaPlus />} />
-      <MenuItem name="Order now" icon={<FaPlus />} />
-      <MenuItem name="Order now" icon={<FaPlus />} />
+      <MenuItem name="Add to cart" icon={<FaPlus />} />
+      <MenuItem name="Add to cart" icon={<FaPlus />} />
+      <MenuItem name="Add to cart" icon={<FaPlus />} />
+      <MenuItem name="Add to cart" icon={<FaPlus />} />
+      <MenuItem name="Add to cart" icon={<FaPlus />} />
     </div>
   );
 }
--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,33 +1,40 @@
+import React from "react";
 import Button from "./Button";
+import { data } from "./mock-data"; // Import the data array
+
 function MenuItem({ name, icon }) {
   return (
     <div>
-      <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-5">
-        <div className="flex items-center gap-5">
-          <img
-            className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
-            src="../src/assets/pizzaBanner.jpeg"
-            alt=""
-          />
-          <div className="flex flex-col justify-start items-start ">
-            <h1 className="text-xl font-bold">Vegan veggie</h1>
-            <p className="py-3 text-sm italic">
-              Daiya vegan pizza, paired with fresh veggies
-            </p>
-            <h1 className="font-bold text-orange-400">$150</h1>
-          </div>
-        </div>
-
-        <div className="mr-5">
-          <Button> 
-            <div className="flex gap-3 align-middle justify-center items-center">
-              {name} 
-              {icon}   
+      {data.map((menuGroup) =>
+        menuGroup.map((item) => (
+          <div
+            key={item.id}
+            className="rounded-lg flex align-middle items-center bg-white justify-between mb-5"
+          >
+            <div className="flex items-center gap-5">
+              <img
+                className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
+                src={item.imageUrl}
+                alt={item.name}
+              />
+              <div className="flex flex-col justify-start items-start ">
+                <h1 className="text-xl font-bold">{item.name}</h1>
+                <p className="py-3 text-sm italic">{item.description}</p>
+                <h1 className="font-bold text-orange-400">${item.price}</h1>
+              </div>
             </div>
-            
-          </Button>
-        </div>
-      </div>
+
+            <div className="mr-5">
+              <Button>
+                <div className="flex gap-3 align-middle justify-center items-center">
+                  {name}
+                  {icon}
+                </div>
+              </Button>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
