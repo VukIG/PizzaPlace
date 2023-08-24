a6279614f8d4e8020885d09a33563e19de2d0ca8
VukIG <vuk.ignjatovic05@gmail.com>
2023-08-24 20:33:20 +0200

    Ispravljene komponente i dodat map umesto foreach

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,15 +1,18 @@
 import MenuItem from "./MenuItem";
 import { FaPlus } from "react-icons/fa";
-
+import data from './mockData';
 function Menu() {
+  console.log(data)
+
   return (
+    
     <div className="w-full flex flex-col justify-start h-full bg-slate-200 p-5 mb-5">
       <h1 className="text-4xl mb-10 font-bold">Discover menu</h1>
-      <MenuItem name="Add to cart" icon={<FaPlus />} />
-      <MenuItem name="Add to cart" icon={<FaPlus />} />
-      <MenuItem name="Add to cart" icon={<FaPlus />} />
-      <MenuItem name="Add to cart" icon={<FaPlus />} />
-      <MenuItem name="Add to cart" icon={<FaPlus />} />
+      {data.map(element => {
+        return element.map( part => {
+          return <MenuItem name="Add to cart" icon={<FaPlus/>} img={part.imageUrl} title={part.name} desc={part.description} key={crypto.randomUUID()} />
+        })
+      })}
     </div>
   );
 }
--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,42 +1,44 @@
 import Button from "./Button";
-import data  from "./mockData"; 
-
-// eslint-disable-next-line react/prop-types
-function MenuItem({ name, icon }) {    
+import PropTypes from 'prop-types';
+function MenuItem({ name, icon, title, desc, img }) {
   return (
     <div>
-      {data.foreach((menuGroup) => (
-        menuGroup.foreach((item) => (
-          <div
-            key={item.id}
-            className="rounded-lg flex align-middle items-center bg-white justify-between mb-5"
-          >
-            <div className="flex items-center gap-5">
-              <img
-                className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
-                src={item.imageUrl}
-                alt={item.name}
-              />
-              <div className="flex flex-col justify-start items-start ">
-                <h1 className="text-xl font-bold">{item.name}</h1>
-                <p className="py-3 text-sm italic">{item.description}</p>
-                <h1 className="font-bold text-orange-400">${item.price}</h1>
-              </div>
-            </div>
-
-            <div className="mr-5">
-              <Button>
-                <div className="flex gap-3 align-middle justify-center items-center">
-                  {name}
-                  {icon}
-                </div>
-              </Button>
-            </div>
+      <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-5">
+        <div className="flex items-center gap-5">
+          <img
+            className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
+            src={img}
+            alt=""
+          />
+          <div className="flex flex-col justify-start items-start ">
+            <h1 className="text-xl font-bold">{title}</h1>
+            <p className="py-3 text-sm italic">
+              {desc}
+            </p>
+            <h1 className="font-bold text-orange-400">$150</h1>
           </div>
-        ))
-      ))}
+        </div>
+
+        <div className="mr-5">
+          <Button> 
+            <div className="flex gap-3 align-middle justify-center items-center">
+              {name} 
+              {icon}   
+            </div>
+            
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
+
+MenuItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+};
 
 export default MenuItem;
