188c64b059261bf0ba1d48f374bf8019a8b7aa0b
VukIG <vuk.ignjatovic05@gmail.com>
2023-09-05 15:33:50 +0200

    Menu page popravljen

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,23 +1,29 @@
-import AmountButton from "./AmountButton";
+import { Link } from 'react-router-dom';
+import AmountButton from './AmountButton';
 
-function RandomItem({ data }) {
-  const { name, description, price, imageUrl } = data; // Destructure the data object
-
+function MenuItem({ data }) {
+  const { name, description, imageUrl, price, id } = data;
   return (
-    <div className="flex gap-5 bg-white flex-col p-5 w-full rounded">
-      <img className="w-full object-cover rounded h-72" src={imageUrl} alt="" />
-      <div className="py-3">
-        <h1 className="text-xl font-bold">{name}</h1>
-        <p className="italic font-normal">{description}</p>
-      </div>
-      <div className="flex w-full justify-between items-center">
-        <p className="text-orange-400 text-xl font-bold">${price}</p>
-        <div>
+    <div>
+      <Link to={`/menu/details/${id}`}>
+        <div className=" rounded-lg flex align-middle items-center bg-white justify-between mb-5">
+          <div className="flex items-center gap-5">
+            <img
+              className="w-[100px] h-[100px] my-5 ml-5 rounded-lg"
+              src={imageUrl}
+              alt=""
+            />
+            <div className="flex flex-col justify-start items-start ">
+              <h1 className="text-xl font-bold">{name}</h1>
+              <p className="py-3 text-sm italic">{description}</p>
+              <h1 className="font-bold text-orange-400">${price}</h1>
+            </div>
+          </div>
           <AmountButton />
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
 
-export default RandomItem;
+export default MenuItem;
