9b807a92acde4e75c9f29815eaf6fb95e10f4305
VukIG <vuk.ignjatovic05@gmail.com>
2023-09-27 21:25:03 +0200

    Query setup u main u

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx';
 import './index.css';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </QueryClientProvider>
+
 );
